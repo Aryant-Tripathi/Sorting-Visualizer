e36b56cb5cc1dd7d9eb5df9b374a5de68135e9b4
Aryant-Tripathi <60316716+Aryant-Tripathi@users.noreply.github.com>
2022-01-21 20:03:25 +0530

    Add files via upload

---
+++ sorting visuliazer in JS/js_files/bubble.js
@@ -0,0 +1,37 @@
+async function bubble()
+{
+    console.log('In bubble()');
+    const ele = document.querySelectorAll(".bar");
+    for(let i = 0; i < ele.length - 1; i++)
+    {
+        console.log('In ith loop');
+        for(let j = 0; j < ele.length - i - 1; j++)
+        {
+            console.log('In jth loop');
+            ele[j].style.background = 'blue';
+            ele[j+1].style.background = 'blue';
+            if(parseInt(ele[j].style.height) > parseInt(ele[j+1].style.height))
+            {
+                console.log('In if condition');
+                await waitforme(delay);
+                swap(ele[j], ele[j+1]);
+
+            }
+            ele[j].style.background = 'cyan';
+            ele[j+1].style.background = 'cyan';
+        }
+        ele[ele.length - i - 1].style.background = 'green';
+    }
+    ele[0].style.background = 'green';
+}
+
+const bubSortbtn = document.querySelector(".bubbleSort");
+bubSortbtn.addEventListener('click', async function(){
+    disableSortingBtn();
+    disableSizeSlider();
+    disableNewArrayBtn();
+    await bubble();
+    enableSortingBtn();
+    enableSizeSlider();
+    enableNewArrayBtn();
+});
